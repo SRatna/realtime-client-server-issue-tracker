e2d5f87eed8e532e4cce2f98696e3a38e553587a
sratna <adhikariratna52@gmail.com>
2023-03-05 14:09:53 +0100

    parse the stories

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+type TaskPayload struct {
+	Id       int `json:"id"`
+	Estimate int `json:"estimate"`
+}
+
+type StoryPayload struct {
+	Id        int           `json:"id"`
+	NoOfTasks int           `json:"noOfTasks"`
+	Completed bool          `json:"completed"`
+	Tasks     []TaskPayload `json:"tasks"`
+}
+
 func main() {
 	app := fiber.New()
 
 	app.Post("api/jobs", func(c *fiber.Ctx) error {
-
+		fmt.Println("cool")
+		story := new(StoryPayload)
+		if err := c.BodyParser(&story); err != nil {
+			fmt.Println(err)
+			return err
+		}
+		fmt.Printf("%v", story)
 		return nil
 	})
 
